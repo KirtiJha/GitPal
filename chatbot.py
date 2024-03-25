0854c66adf301b9db31b212ea3d1f3d85aea23a7
Kirti Jha <kirtijha@in.ibm.com>
2024-03-25 18:18:32 +0530

    initial commit

---
+++ chatbot.py
@@ -0,0 +1,64 @@
+import streamlit as st
+import utils
+import time
+import os
+
+# local = False
+# if local:
+#     from dotenv import load_dotenv
+
+#     load_dotenv()
+
+st.title("Chat-with-your-codebase")
+
+## Get some user inputs
+# if local:
+#     gen_ai_key = os.environ.get("GEN_AI_KEY")
+# else:
+gen_ai_key = st.text_input("Enter your IBM WatsonX API Key", "")
+if gen_ai_key:
+    # os.environ["GEN_AI_KEY"] = gen_ai_key
+
+    user_repo = st.text_input(
+        "Github Link to your public codebase",
+        "",
+    )
+    if user_repo:
+        st.write("You entered:", user_repo)
+
+        ## Load the Github Repo
+        embedder = utils.Embedder(user_repo)
+        embedder.clone_repo()
+        st.write("Your repo has been cloned")
+
+        ## Chunk and Create DB
+        st.write("Parsing the content and embedding it. This may take some time")
+        conversation_chain = embedder.get_conversation_chain(gen_ai_key=gen_ai_key)
+        st.write("Done Loading. Ready to take your questions")
+
+        # Initialize chat history
+        if "messages" not in st.session_state:
+            st.session_state.messages = []
+
+        # Display chat messages from history on app rerun
+        for message in st.session_state.messages:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+
+        # Accept user input
+        if prompt := st.chat_input("Type your question here."):
+            # Add user message to chat history
+            st.session_state.messages.append({"role": "user", "content": prompt})
+            # Display user message in chat message container
+            with st.chat_message("user"):
+                st.markdown(prompt)
+            with st.spinner("Thinking...."):
+                # Display assistant response in chat message container
+                response = embedder.retrieve_results(prompt, conversation_chain)
+                # Display assistant response in chat message container
+                with st.chat_message("assistant"):
+                    st.markdown(response)
+                # Add assistant response to chat history
+                st.session_state.messages.append(
+                    {"role": "assistant", "content": response}
+                )
---
+++ utils.py
@@ -0,0 +1,146 @@
+from langchain.document_loaders import TextLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import git
+import os
+from queue import Queue
+
+local = False
+if local:
+    from dotenv import load_dotenv
+
+    load_dotenv()
+
+from genai import Client, Credentials
+from genai.extensions.langchain import LangChainEmbeddingsInterface
+from genai.schema import TextEmbeddingParameters
+from genai.extensions.langchain.chat_llm import LangChainChatInterface
+from genai.schema import (
+    DecodingMethod,
+    ModerationHAP,
+    ModerationParameters,
+    TextGenerationParameters,
+    TextGenerationReturnOptions,
+)
+
+from langchain.vectorstores.faiss import FAISS
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import ConversationalRetrievalChain
+
+model_name = "sentence-transformers/all-MiniLM-L6-v2"
+model_kwargs = {"device": "cpu"}
+allowed_extensions = [".py", ".ipynb", ".md"]
+
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
+
+
+class Embedder:
+    def __init__(self, git_link) -> None:
+        self.git_link = git_link
+        last_name = self.git_link.split("/")[-1]
+        self.clone_path = last_name.split(".")[0]
+        self.MyQueue = Queue(maxsize=2)
+
+    def add_to_queue(self, value):
+        if self.MyQueue.full():
+            self.MyQueue.get()
+        self.MyQueue.put(value)
+
+    def clone_repo(self):
+        if not os.path.exists(self.clone_path):
+            # Clone the repository
+            git.Repo.clone_from(self.git_link, self.clone_path)
+
+    def extract_all_files(self):
+        root_dir = self.clone_path
+        self.docs = []
+        for dirpath, dirnames, filenames in os.walk(root_dir):
+            for file in filenames:
+                file_extension = os.path.splitext(file)[1]
+                if file_extension in allowed_extensions:
+                    try:
+                        loader = TextLoader(
+                            os.path.join(dirpath, file), encoding="utf-8"
+                        )
+                        self.docs.extend(loader.load_and_split())
+                    except Exception as e:
+                        pass
+        self.delete_directory(self.clone_path)
+        return self.docs
+
+    def chunk_files(self, docs):
+        text_splitter = RecursiveCharacterTextSplitter(
+            separators=["\n\n", "\n", " ", ""], chunk_size=1000, chunk_overlap=200
+        )
+        chunked_documents = text_splitter.split_documents(docs)
+        print("Number of chunks:", len(chunked_documents), "\n\n")
+        return chunked_documents
+
+    def delete_directory(self, path):
+        if os.path.exists(path):
+            for root, dirs, files in os.walk(path, topdown=False):
+                for file in files:
+                    file_path = os.path.join(root, file)
+                    os.remove(file_path)
+                for dir in dirs:
+                    dir_path = os.path.join(root, dir)
+                    os.rmdir(dir_path)
+            os.rmdir(path)
+
+    def get_conversation_chain(self, gen_ai_key):
+        credentials = Credentials(api_key=gen_ai_key)
+        client = Client(credentials=credentials)
+        # Create vector db
+        docs = self.extract_all_files()
+        chunked_documents = self.chunk_files(docs)
+        embeddings = LangChainEmbeddingsInterface(
+            client=client,
+            model_id="sentence-transformers/all-minilm-l6-v2",
+            parameters=TextEmbeddingParameters(truncate_input_tokens=True),
+        )
+        vector_store = FAISS.from_documents(
+            documents=chunked_documents, embedding=embeddings
+        )
+        #  Create conversation chain
+        memory = ConversationBufferMemory(
+            memory_key="chat_history", return_messages=True
+        )
+        credentials = Credentials(
+            api_key=gen_ai_key,
+            api_endpoint="https://bam-api.res.ibm.com/v2/text/chat?version=2024-01-10",
+        )
+        client = Client(credentials=credentials)
+
+        llm = LangChainChatInterface(
+            model_id="meta-llama/llama-2-70b-chat",
+            client=client,
+            parameters=TextGenerationParameters(
+                decoding_method=DecodingMethod.SAMPLE,
+                max_new_tokens=4090,
+                min_new_tokens=10,
+                temperature=0.5,
+                top_k=50,
+                top_p=1,
+                return_options=TextGenerationReturnOptions(
+                    input_text=False, input_tokens=True
+                ),
+            ),
+            moderations=ModerationParameters(
+                # Threshold is set to very low level to flag everything (testing purposes)
+                # or set to True to enable HAP with default settings
+                hap=ModerationHAP(input=True, output=False, threshold=0.01)
+            ),
+        )
+
+        conversation_chain = ConversationalRetrievalChain.from_llm(
+            llm=llm, retriever=vector_store.as_retriever(), memory=memory
+        )
+        self.delete_directory(self.clone_path)
+        return conversation_chain
+
+    def retrieve_results(self, query, conversation_chain):
+        chat_history = list(self.MyQueue.queue)
+        # qa = self.get_conversation_chain(vector_store, gen_ai_key)
+        result = conversation_chain({"question": query, "chat_history": chat_history})
+        self.add_to_queue((query, result["answer"]))
+        return result["answer"]
