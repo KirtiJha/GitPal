ed34b623eb0132b7110097f7a6dbfb72e7f1356c
Kirti Nath Jha <kirtijha@in.ibm.com>
2024-06-09 11:33:50 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -6,6 +6,7 @@ from queue import Queue
 
 # from prompts import model_prompt, custom_question_prompt
 # no using custom system prompt. I will use default system prompt
+# adding comment to test webhook
 from prompts_llama3 import model_prompt, custom_question_prompt
 
 from genai import Client, Credentials
